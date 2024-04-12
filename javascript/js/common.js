6e70a1b851866e56072b2026da6874428a941d06
theeasylearn <theeasylearn@gmail.com>
2024-04-12 08:16:46 +0530

    reusable functions

--- javascript/js/common.js
+++ javascript/js/common.js
@@ -2,7 +2,12 @@
 let $ = function(tagId){
     return document.getElementById(tagId);
 }
-let val = function(tagId)
+let val = function(tagId,content=null)
 {
-    return $(tagId).value;
+    if(content === null)
+        return $(tagId).value;
+    else 
+        $(tagId).value = content;
 }
+let html = (tagId,content) => $(tagId).innerHTML = content;
+let int = (tagId) => parseInt(val(tagId));

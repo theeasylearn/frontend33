26afadc5b8b65e5c9b20e5e3c2e031a781fbd11a
theeasylearn <theeasylearn@gmail.com>
2024-05-13 08:25:38 +0530

    reactjs day 3

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let num1 = 30;
+let num2 = 7;
+var page = (
+    <div>
+        <h1 className='heading'>3rd example in Reactjs</h1>
+        <ul>
+            <li>addition of {num1} and {num2}  = {num1 + num2}</li>
+            <li>subtraction of {num1} and {num2}  = {num1 - num2}</li>
+            <li>multiplication of {num1} and {num2}  = {num1 * num2}</li>
+            <li>division of {num1} and {num2}  = {(num1 / num2).toFixed(2)}</li>
+        </ul>
+    </div>
+)
+root.render(page);

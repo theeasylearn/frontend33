ae73a1c60a16494db005cc994ede71691a230093
theeasylearn <theeasylearn@gmail.com>
2024-05-29 08:28:55 +0530

    index

--- reactjs/app1/src/DinningTable.js
+++ reactjs/app1/src/DinningTable.js
@@ -1,53 +1,46 @@
 import React from 'react';
 export default class DinningTable extends React.Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         //property variable
         this.guest = props.guest;
         this.no = props.no;
         //create state object
         this.state = {
-            thali:0,
-            ras:0,
-            papad:0,
-            roti:0,
-            chas:0,
-            total:0
+            thali: 0,
+            ras: 0,
+            papad: 0,
+            roti: 0,
+            chas: 0,
+            total: 0
         }
-        
+
     }
     //create static final variable
     static get THALI_PRICE() {
         return 100;
     }
-    static get ROTI_PRICE()
-    {
+    static get ROTI_PRICE() {
         return 10;
     }
-    static get RAS_PRICE()
-    {
+    static get RAS_PRICE() {
         return 25;
     }
-    static get CHAS_PRICE()
-    {
+    static get CHAS_PRICE() {
         return 15;
     }
-    static get PAPAD_PRICE()
-    {
+    static get PAPAD_PRICE() {
         return 7;
     }
 
     //create event handler arrow function
-    updateThali =()=>
-    {
+    updateThali = () => {
         // this.state.thali = this.state.thali + 1;
         this.setState({
             thali: this.state.thali + 1,
         });
     }
-    updateRas = () =>
-    {
+    updateRas = () => {
         this.setState({
             ras: this.state.ras + 1
         });
@@ -55,7 +48,7 @@ export default class DinningTable extends React.Component {
     }
     updateRoti = () => {
         this.setState({
-            roti:this.state.roti + 1
+            roti: this.state.roti + 1
         });
     }
     updatePapad = () => {
@@ -65,8 +58,30 @@ export default class DinningTable extends React.Component {
     }
     updateChas = () => {
         this.setState({
-            chas:this.state.chas + 1
+            chas: this.state.chas + 1
         });
+    }
+    shouldComponentUpdate(nextProperty, nextState) {
+        console.log('shouldComponentUpdate method is called....');
+        console.log(nextState);
+        if (this.state.roti != nextState.roti || this.state.papad != nextState.papad || this.state.chas != nextState.chas || this.state.ras !=nextState.ras || this.state.thali != nextState.thali)
+            return true;
+        else
+            return false;
+    }
+    componentWillUpdate(nextProperty, nextState) {
+        console.log('componentWillUpdate method is called....');
+
+    }
+    componentDidUpdate(previousProperty, previousState) {
+        console.log('componentDidUpdate method is called....');
+        if (previousState.total==0 || previousState.total!=this.state.total) {
+            let temp = (this.state.chas * DinningTable.CHAS_PRICE) + (this.state.papad * DinningTable.PAPAD_PRICE) + (this.state.ras * DinningTable.RAS_PRICE) + (this.state.roti * DinningTable.ROTI_PRICE) + (this.state.thali * DinningTable.THALI_PRICE);
+            console.log(temp);
+            this.setState({
+                total: temp
+            });
+        }
     }
     render() {
         return (<div className="col-lg-3 mb-3">
--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,56 +1,7 @@
 /* importing required react components */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-class FaceBookPost  extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        console.log('constructor is called....');
-        this.state = {
-          label: 'Like'
-        }
-    }
-    componentWillMount()
-    {
-        console.log('component will mount.');
-    }
-    componentDidMount()
-    {
-      console.log('component did mounted.');
-    }
-    updateLike = () =>
-    {
-        this.setState({
-          label:'Liked'
-        });
-    }
-    render()
-    {
-        console.log('render method is called.');
-         return (<div className="container">
-         <div className="row">
-           <div className="col-lg-3">
-             <div className="card">
-               <img src="https://picsum.photos/200" alt className="card-img-top" />
-               <div className="card-body">
-                 <span className="d-flex justify-content-between">
-                   <h5>Priya Mehta</h5>
-                   <button onClick={this.updateLike} type='button' className="btn btn-primary btn-sm">{this.state.label}</button>
-                 </span>
-                 <p className="fw-bold border-bottom">Comments</p>
-                 <p className="m-0">you clicked good photo.</p>
-                 <p className="m-0">Awasome</p>
-                 <textarea className="form-control" defaultValue={""} />
-                 <button type='button' className="btn btn-primary btn-sm mt-1">Post</button>
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
-       );
-    }
-}
+import Resturant from './Resturant';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FaceBookPost />)
+
+root.render(<Resturant />)

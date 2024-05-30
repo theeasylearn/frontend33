29de25ac38ac3023a0083797bfe74d29ec105ed4
theeasylearn <theeasylearn@gmail.com>
2024-05-30 08:05:15 +0530

    app life cycle

--- reactjs/app1/src/DinningTable.js
+++ reactjs/app1/src/DinningTable.js
@@ -12,7 +12,8 @@ export default class DinningTable extends React.Component {
             papad: 0,
             roti: 0,
             chas: 0,
-            total: 0
+            total: 0,
+            isDisabled:false
         }
 
     }
@@ -61,27 +62,28 @@ export default class DinningTable extends React.Component {
             chas: this.state.chas + 1
         });
     }
-    shouldComponentUpdate(nextProperty, nextState) {
-        console.log('shouldComponentUpdate method is called....');
-        console.log(nextState);
-        if (this.state.roti != nextState.roti || this.state.papad != nextState.papad || this.state.chas != nextState.chas || this.state.ras !=nextState.ras || this.state.thali != nextState.thali)
-            return true;
-        else
-            return false;
-    }
     componentWillUpdate(nextProperty, nextState) {
-        console.log('componentWillUpdate method is called....');
+        // console.log('componentWillUpdate method is called....');
 
     }
     componentDidUpdate(previousProperty, previousState) {
-        console.log('componentDidUpdate method is called....');
-        if (previousState.total==0 || previousState.total!=this.state.total) {
+        // console.log('componentDidUpdate method is called....');
+        // console.log("Current State",this.state);
+        // console.log("Current State",previousState);
+        if (this.state.thali != previousState.thali || this.state.ras != previousState.ras || this.state.roti != previousState.roti || this.state.chas != previousState.chas || this.state.papad != previousState.papad) {
             let temp = (this.state.chas * DinningTable.CHAS_PRICE) + (this.state.papad * DinningTable.PAPAD_PRICE) + (this.state.ras * DinningTable.RAS_PRICE) + (this.state.roti * DinningTable.ROTI_PRICE) + (this.state.thali * DinningTable.THALI_PRICE);
             console.log(temp);
             this.setState({
                 total: temp
             });
+            if(this.state.thali == 4)
+            {
+                this.setState({
+                    isDisabled:true
+                });
+            }    
         }
+
     }
     render() {
         return (<div className="col-lg-3 mb-3">
@@ -93,7 +95,7 @@ export default class DinningTable extends React.Component {
                 <div className="card-body">
                     <div className="row">
                         <div className="col-12 mb-2">
-                            <button onClick={this.updateThali} type="button" className="btn btn-primary w-100">Thali <span className="badge text-bg-secondary">{this.state.thali}</span></button>
+                            <button disabled={this.state.isDisabled} onClick={this.updateThali} type="button" className="btn btn-primary w-100">Thali <span className="badge text-bg-secondary">{this.state.thali}</span></button>
                         </div>
                         <div className="col-6 mb-2">
                             <button onClick={this.updateRas} type="button" className="w-100 btn btn-warning">Ras <span className="badge text-bg-secondary">{this.state.ras}</span></button>

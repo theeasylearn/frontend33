add708dc8e4c24a6241a9aa3cc8c6c971daeeb7d
theeasylearn <theeasylearn@gmail.com>
2024-07-11 08:30:30 +0530

    design

---
+++ reactjs/app3/public/shop/assets/js/vendors/validation.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var a=document.querySelectorAll(".needs-validation");Array.from(a).forEach(e=>{e.addEventListener("submit",a=>{e.checkValidity()||(a.preventDefault(),a.stopPropagation()),e.classList.add("was-validated")},!1)})})();
---
+++ reactjs/app3/public/shop/assets/js/vendors/zoom.js
@@ -0,0 +1,1 @@
+function zoom(f){var t=f.currentTarget;offsetX=f.offsetX||f.touches[0].pageX,f.offsetY?offsetY=f.offsetY:offsetX=f.touches[0].pageX,x=offsetX/t.offsetWidth*100,y=offsetY/t.offsetHeight*100,t.style.backgroundPosition=x+"% "+y+"%"}
